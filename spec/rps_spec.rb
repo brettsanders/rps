f1d02181519ade0ce16638a02f629149572099a1
Brett Sanders <brettrsanders@gmail.com>
2014-08-25 23:42:02 -0500

    add failing tests for RPS rules (winner,loser,tie)

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -10,20 +10,62 @@ describe "Rock Paper Scissors" do
     describe "When player throws rock" do
       it "and computer throws rock" do
         @rps.play('rock', 'rock')
+        expect(@rps.winner).to eq false
+        expect(@rps.loser).to eq false
         expect(@rps.tie).to eq true
+      end
+      it "and computer throws paper" do
+        @rps.play('rock', 'paper')
+        expect(@rps.winner).to eq :computer
+        expect(@rps.loser).to eq :player
+        expect(@rps.tie).to eq false
+      end
+      it "and computer throws scissors" do
+        @rps.play('rock', 'scissors')
+        expect(@rps.winner).to eq :computer
+        expect(@rps.loser).to eq :player
+        expect(@rps.tie).to eq false
       end
     end
 
     describe "When player throws paper" do
+      it "and computer throws rock" do
+        @rps.play('paper', 'rock')
+        expect(@rps.winner).to eq :player
+        expect(@rps.loser).to eq :computer
+        expect(@rps.tie).to eq false
+      end
       it "and computer throws paper" do
         @rps.play('paper', 'paper')
+        expect(@rps.winner).to eq false
+        expect(@rps.loser).to eq false
         expect(@rps.tie).to eq true
+      end
+      it "and computer throws scissors" do
+        @rps.play('paper', 'scissors')
+        expect(@rps.winner).to eq :computer
+        expect(@rps.loser).to eq :player
+        expect(@rps.tie).to eq false
       end
     end
 
     describe "When player throws scissors" do
+      it "and computer throws rock" do
+        @rps.play('scissors', 'rock')
+        expect(@rps.winner).to eq :computer
+        expect(@rps.loser).to eq :player
+        expect(@rps.tie).to eq false
+      end
+      it "and computer throws paper" do
+        @rps.play('scissors', 'paper')
+        expect(@rps.winner).to eq :player
+        expect(@rps.loser).to eq :computer
+        expect(@rps.tie).to eq false
+      end
       it "and computer throws scissors" do
         @rps.play('scissors', 'scissors')
+        expect(@rps.winner).to eq :false
+        expect(@rps.loser).to eq :false
         expect(@rps.tie).to eq true
       end
     end
